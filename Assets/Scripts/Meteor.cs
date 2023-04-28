30de413b6bb30e36f1c71d9335db207c29d0a136
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2023-04-28 20:14:32 -0300

    Enemy : Meteor -> using Inheritance. Meteor Script

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Meteor : Enemy
 {
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float maxSpeed;
+    private float speed;
+
     void Start()
     {
-        
+        speed = Random.Range(minSpeed, maxSpeed);
+        rb.velocity = Vector2.down * speed;
     }
 
-    void Update() 
+    void Update()
     {
-        
+
+    }
+
+    public override void HurtSequence()
+    {
+        // do something
+    }
+    public override void DeathSequence()
+    {
+        // do something
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            Destroy(collider.gameObject);
+        }
     }
 }
